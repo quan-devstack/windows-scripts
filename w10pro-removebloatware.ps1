ff55e0653a90fb5ed7e1514d0db3621d192c4600
Quan2409 <dokahcquan2002@gmail.com>
2025-10-20 14:05:40 +0700

    20/10/25 upldate bloatware to remove

--- w10pro-removebloatware.ps1
+++ w10pro-removebloatware.ps1
@@ -35,7 +35,8 @@ $BloatApps = @(
     "Microsoft.OneNote",
     "Microsoft.MicrosoftJournal",
     "Microsoft.Wallet",
-    "Microsoft.ScreenSketch"
+    "Microsoft.ScreenSketch",
+    "Microsoft.LinkedIn"
 )
 
 # Gỡ từng App
@@ -48,18 +49,21 @@ foreach ($app in $BloatApps) {
 Write-Host "=== Bloatware Removed! ===" -ForegroundColor Green
 
 # Dọn dẹp các package còn sót
-Write-Host "Cleaning AppX Package..." -ForegroundColor Cyan
-Get-AppxPackage | Where-Object {$_.NonRemovable -eq $false} | Remove-AppPackage -ErrorAction SilentlyContinue
+# Write-Host "Cleaning AppX Package..." -ForegroundColor Cyan
+# Get-AppxPackage | Where-Object {$_.NonRemovable -eq $false} | Remove-AppPackage -ErrorAction SilentlyContinue
 
 # Xóa quảng cáo & đề xuất trong Start Menu / Settings
 Write-Host "Disable Adversiting & Suggestions..." -ForegroundColor Cyan
 
 # Tắt đề xuất trong Start Menu
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+
 # Tắt quảng cáo trong Settings
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
+
 # Tắt tips
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement" /v ScoobeSystemSettingEnabled /t REG_DWORD /d 0 /f
+
 # Tắt Microsoft Consumer Experience
 reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableConsumerAccountStateContent /t REG_DWORD /d 1 /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
